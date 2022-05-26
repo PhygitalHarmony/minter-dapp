680fb9889ae86541cf1ebc1b32df2efc79d965ea
PhygitalHarmony <105824021+PhygitalHarmony@users.noreply.github.com>
2022-05-26 15:12:45 -0500

    commit

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xfca83642941613Dbb01A732d8Ec59C04fea35d34"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Phygital Harmony NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'Phygital Harmony is an NFT project merging the physical + digital (phygital) world. Ownership provides benefits to shaping land IRL into a destination to visit. Maintaining a connection with the physical world through art and beauty.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
